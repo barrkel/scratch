d70d956437041798f9bec92b962aac77f99de161
Barry Kelly <barry.j.kelly@gmail.com>
2013-09-14 02:15:22 +0100

    Improve line endings normalization, and add scratchlog dumper prototype

---
+++ scratchlog/Program.cs
@@ -0,0 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Barrkel.ScratchPad;
+using System.IO;
+
+namespace ScratchLog
+{
+	class Program
+	{
+		static int Main(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("usage: {0} <root-dir>", Path.GetFileNameWithoutExtension(
+					Environment.GetCommandLineArgs()[0]));
+				Console.WriteLine("Output date-stamped log of modifications in order with titles at time of modification");
+				return 1;
+			}
+			ScratchRoot root = new ScratchRoot(args[0]);
+			
+			var updates = new List<Update>();
+			
+			foreach (ScratchBook book in root.Books)
+			{
+				foreach (ScratchPage page in book.Pages)
+				{
+					ScratchIterator iter = page.GetIterator();
+					iter.MoveToEnd();
+					do {
+						updates.Add(new Update { Title = new StringReader(iter.Text).ReadLine(), Stamp = iter.Stamp });
+					} while (iter.MovePrevious());
+				}
+			}
+			
+			Update previous = null;
+			Update finish = null;
+			int updateCount = 0;
+			foreach (var update in updates.OrderByDescending(x => x.Stamp).Where(x => x.Title != null))
+			{
+				if (previous == null)
+				{
+					previous = update;
+					continue;
+				}
+				
+				if (previous.Title == update.Title && (previous.Stamp - update.Stamp).TotalHours < 1)
+				{
+					// within the hour => probably the same task
+					if (finish == null)
+					{
+						// this is the start of a range
+						finish = previous;
+						updateCount = 1;
+					}
+					else
+					{
+						++updateCount;
+					}
+				}
+				else
+				{
+					if (finish != null)
+					{
+						// we've come to the start of a range, and previous was the beginning
+						TimeSpan duration = finish.Stamp - previous.Stamp;
+						Console.WriteLine("{0} {1} ({2}) {3}", previous.Stamp, NiceDuration(duration), updateCount, previous.Title);
+					}
+					else
+					{
+						// different task that previous, and not part of range
+						Console.WriteLine("{0} {1}", previous.Stamp, previous.Title);
+					}
+
+					updateCount = 0;
+					finish = null;
+				}
+				
+				previous = update;
+			}
+			if (finish != null)
+			{
+				// we've come to the start of a range, and previous was the beginning
+				TimeSpan duration = finish.Stamp - previous.Stamp;
+				Console.WriteLine("{0} {1} ({2}) {3}", previous.Stamp, NiceDuration(duration), updates, previous.Title);
+			}
+			else
+			{
+				// different task that previous, and not part of range
+				Console.WriteLine("{0} {1}", previous.Stamp, previous.Title);
+			}
+			
+			
+			return 0;
+		}
+		
+		static string NiceDuration(TimeSpan span)
+		{
+			if (span.TotalDays > 1)
+				return string.Format("{0} days {1} hours", (int) span.TotalDays, span.Hours);
+			if (span.TotalHours > 1)
+				return string.Format("{0} hours {1} mins", (int) span.TotalHours, span.Minutes);
+			return string.Format("{0} minutes", (int) span.TotalMinutes);
+		}
+		
+		class Update
+		{
+			public DateTime Stamp { get; set; }
+			public string Title { get; set; }
+		}
+	}
+}
